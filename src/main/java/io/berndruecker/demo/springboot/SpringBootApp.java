533e413157c721478a1e4836d879ff1fa3cae581
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-07-11 16:56:36 +0200

    Updated to latest version and added proper Spring Boot Repackaging

--- src/main/java/io/berndruecker/demo/springboot/SpringBootApp.java
+++ src/main/java/io/berndruecker/demo/springboot/SpringBootApp.java
@@ -1,18 +1,16 @@
 package io.berndruecker.demo.springboot;
 
-import org.camunda.community.rest.client.springboot.CamundaApi;
-import org.camunda.community.rest.client.springboot.CamundaOpenApiStarter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootApp {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootApp.class, args);
-    }
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootApp.class, args);
 
+        // For playing around it might be nice to just start a process right away
+        // context.getBean(ExampleRestEndpoint.class).startProcess(null);
+    }
 }
